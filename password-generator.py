2077d9741fb3cc8e299baaff99b2e7d3185a3355
sidd0894 <112334974+sidd0894@users.noreply.github.com>
2025-03-06 09:23:06 +0530

    refactored password generator with argparse and secrets module

--- password-generator.py
+++ password-generator.py
@@ -1,91 +1,33 @@
-import random
+import secrets
+import string
+import argparse
 import pyperclip
 
-lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
-upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-nums = '0123456789'
-chars = '!@#$$%&_'
-stringList = [lowerCaseLetters, upperCaseLetters, nums, chars]
-length = ''
-numberOfPass = ''
-passwordList = []
-
-
-def getUserInput(prompt: str, default, min):
-    while True:
-        try:
-            num = input(prompt).strip()
-            if num == '':
-                num = default
-                break
-
-            elif int(num) < min:
-                print(f'Minimum value can be {min}')
-
-            else:
-                num = int(num)
-                break
-
-        except ValueError:
-            print('Invalid input.')
-
-    return num
-
-
-
-
-def generatePass(length):
-    password = ''
-    for i in range(0, length):
-        element = stringList[random.randint(0, len(stringList)-1)]
-        password += element[random.randint(0, len(element)-1)]
-    return password
-
-
-
-
 def main():
-    print('\n(NOTE - Leave empty to use default values.)')
+    password = ''
+    stringList = [string.ascii_lowercase, string.ascii_uppercase, string.digits, '!@#$%&*_']
 
-    length = getUserInput('Length of password (default is 8): ', 8, 4)
-    numberOfPass = getUserInput('Number of passwords to generate (default is 1): ', 1, 1)
-    
-    for i in range(0, numberOfPass):
-        password = generatePass(length)
-        passwordList.append(password)
-        print(f'{i+1}.  {password}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--length', '-len', type=int, default=16, help='length of password', metavar='')
+    parser.add_argument('--pass-count', '-count', type=int, default=1, help='number of passwords to generate', metavar='')
+    parser.add_argument('--show-pass', '-show', action='store_true', help='use this flag to show password')
+    args = parser.parse_args()
 
+    if args.length < 8:
+        parser.error('Minimum password length should be 8')
+    if args.pass_count < 1:
+        parser.error('Pass count should be 1 or greater')
 
-    if len(passwordList) > 0:
-        while True:
-            try:
-                passwordIndex = int(input(f'Enter serial number of password to copy: ').strip())
-                passwordIndex = passwordIndex - 1
+    for i in range(args.pass_count):
+        password += ''.join([secrets.choice(secrets.choice(stringList)) for i in range(args.length)])
+        password += '\n' if args.pass_count > 1 else ''
 
-                if passwordIndex >= 0 and passwordIndex < len(passwordList):
-                    try:
-                        pyperclip.copy(passwordList[passwordIndex]) 
-                        print('Copied !!')
+    if args.show_pass:
+        print(password)
 
-                    except Exception as e:
-                        print(f'[ERROR]: {e}')
-
-                    break
-
-                else:
-                    print(f'[ERROR] No password present at serial number {passwordIndex + 1}')
-
-            except KeyboardInterrupt:
-                print('\nKeyboard interruption. Exiting...')
-                break
-            except:
-                print('[ERROR] Invalid input')
-
-
+    pyperclip.copy(password)
+    print('Password copied !!' if args.pass_count == 1 else 'Copied all passwords !!')
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('\nKeyboard interruption. Exiting...')
+    main()
